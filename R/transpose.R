32affebb76270a03af81e03e75ddec460387e945
Johnson, Randall (NIH/NCI) [C] <johnsonra@mail.nih.gov>
2017-10-19 11:43:16 -0400

    Added transpose_vector
    Also fixed a warning about needing to include testthat in DESCRIPTION

--- R/transpose.R
+++ R/transpose.R
@@ -13,6 +13,38 @@ transpose_loop <- function(x)
       tx[j,i] <- x[i,j]
     }
   }
-    
-  return(tx) 
+
+  return(tx)
+}
+
+# --solutions--
+# transpose x using vectorized notation (i.e. not a nested for loop)
+transpose_vector <- function(x)
+{
+    # using a *single* for loop
+    # tx <- matrix(ncol = dim(x)[1], nrow = dim(x)[2])
+    #
+    # for(i in 1:dim(x)[1])
+    # {
+    #     tx[,i] <- x[i,]
+    # }
+
+    # using sapply()
+    tx <- sapply(1:dim(x)[1], function(i) x[i,])
+
+    # using apply()
+    # tx <- apply(x, 1, identity)
+
+    # using map2()
+    # tx <- matrix(ncol = dim(x)[1], nrow = dim(x)[2])
+    #
+    # tx[,] <- map2(rep(1:dim(x)[1], each = dim(x)[2]),
+    #               rep(1:dim(x)[2], dim(x)[1]),
+    #               ~ x[.x, .y]) %>%
+    #          unlist()
+
+    # using matrix() directly
+    # tx <- matrix(as.vector(x), ncol = dim(x)[1], nrow = dim(x)[2], byrow = TRUE)
+
+    return(tx)
 }
